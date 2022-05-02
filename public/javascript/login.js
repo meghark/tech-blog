ecd4eca540b5eb4f7604b61b5df993f723b8ec87
meghark <98137756+meghark@users.noreply.github.com>
2022-05-01 22:50:37 -0700

    Wired up login to backend

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,12 +1,30 @@
 
-const loginFormHandler =() =>{
+const loginFormHandler =async () =>{
 
     event.preventDefault();
 
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#pass-login').value.trim();
 
+    if(email && password)
+    {
+        const result = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
 
+        if(result.ok)
+        {
+            console.log('Login Successfull');
+        }
+        else{
+            console.log(result.statusText);
+        }
+    }
 };
 
 //Method to add a new user
@@ -30,7 +48,15 @@ const signUpFormHandler =async () => {
             headers: {'Content-Type': 'application/json'}
         });
 
-        console.log(result);
+        if(result.ok)
+        {
+            console.log('User sign up completed successully');
+        }
+        else{
+            console.log(result.statusText);
+        }
+
+        
     }
 };
 
