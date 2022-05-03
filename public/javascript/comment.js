3806a393c109123870d2a398e82d1165d981384a
meghark <98137756+meghark@users.noreply.github.com>
2022-05-02 21:42:28 -0700

    Added UI handler for comment creation

---
+++ public/javascript/comment.js
@@ -0,0 +1,38 @@
+const commentFormHandler = async() => {
+    event.preventDefault();
+
+    const note = document.querySelector('.comment-message').value.trim();
+    //The url contains the post id. Split the url by / then
+    //get the item in last index
+    const post_id = window.location.toString()
+                        .split('/')[window.location.toString().split('/').length - 1];
+
+    //Saving a new post record also requires the user_id, the api method
+    //will have access to session information with user details
+    if(comment)
+    {
+        const response = fetch('/api/comments',{
+                method: 'POST',
+                body: JSON.stringify({
+                    post_id,
+                    note
+                }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.ok)
+        {
+            document.location.reload();
+        }
+        else{
+            console.log(response.statusText);
+        }
+
+    }
+
+};
+
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
